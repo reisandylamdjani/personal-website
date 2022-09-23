ba1c9679f91681fb744c9ca2dcad607f4a9599cc
reisandylamdjani <lamdjani@gmail.com>
2022-09-23 16:09:58 -0700

    Fixed resume link

--- src/components/Sidebar/sidebar.js
+++ src/components/Sidebar/sidebar.js
@@ -58,7 +58,7 @@ const Sidebar = () => {
                 <a 
                     target = "_blank"
                     rel = "noreferrer"
-                    href = "https://bit.ly/publicResume">
+                    href = "https://drive.google.com/file/d/1NGrrNS6w-TZkOmkT4XKnP5TH_aS-QZno/view?usp=sharing">
                     <FontAwesomeIcon icon={faFile} color = "#4d4d4e" />
                 </a>
             </li>
